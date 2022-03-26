55c9dcf953ad4c6da6410f48911b1d53427246ea
brisfekousis <brisard.fekousis@chambers.com>
2022-03-26 08:43:54 +0000

    headers

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,9 +1,9 @@
-const contractAddress = "0xbA0c3b5A3fb7447f4d84598804C963e0436E57b0"; //Replace with your own contract address
+const contractAddress = "0xba0c3b5a3fb7447f4d84598804c963e0436e57b0"; //Replace with your own contract address
 const chain = 'rinkeby'; // rinkeby or polygon
 
-const welcome_h1 = "Welcome to the CodeCats NFT Project!!";
+const welcome_h1 = "Welcome to the Weird Warthogs NFT Project!!";
 const welcome_h2 = "Connect to MetaMask to Get Started";
-const welcome_p = 'The CodeCats NFT Project is a decentralized, open-source project that aims to demonstrate how to develope and launch your own NFT Collection. Follow step by step on the <a href="https://youtube.com/codestackr"  target="_blank" rel="noopener noreferrer">codeSTACKr</a> YouTube channel.';
+const welcome_p = 'Weird Warthogs is a limited collection of 10,000 unique, programmatically generated NFTs hosted on the Polygon blockchain. Each Weird Warthog is unique - just like you and programmatically generated from 8 properties of varying rarities, from common to ultra rare. Possible traits, including various backgrounds, clothing, earrings and more!';
 const h1_presale_coming_soon = "NFT Drop Coming Soon!!";
 const h1_presale_mint = "Pre-Sale Minting Open!!";
 const h1_public_mint = "Public Minting Open!!";
